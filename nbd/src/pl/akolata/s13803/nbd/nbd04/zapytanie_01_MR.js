3ba53fbf645d07b080c3e3a74b3ecdc6b201c668
Aleksander Kołata <olek.tbg.konta@gmail.com>
2021-04-17 22:00:10 +0200

    [NBD-04] working on exercise 1 with mapReduce - finished

--- nbd/src/pl/akolata/s13803/nbd/nbd04/zapytanie_01_MR.js
+++ nbd/src/pl/akolata/s13803/nbd/nbd04/zapytanie_01_MR.js
@@ -1,11 +1,20 @@
 var mapFunctionNbd0401 = function () {
     var key = this.sex;
-    var value = parseFloat(this.weight);
+    var value = {
+        avgWeight: parseFloat(this.weight),
+        avgHeight: parseFloat(this.height)
+    };
     return emit(key, value);
 };
 
 var reduceFunctionNbd0401 = function (key, value) {
-    return Array.sum(value)/value.length;
+    // value: Array<{avgWeight: number, avgHeight: number}>
+    var sumOfWeight = value.map(obj => obj.avgWeight);
+    var sumOfHeight = value.map(obj => obj.avgHeight);
+    return {
+        avgWeight: Array.sum(sumOfWeight) / value.length,
+        avgHeight: Array.sum(sumOfHeight) / value.length
+    };
 };
 
-printjson(db.people.mapReduce(mapFunctionNbd0401, reduceFunctionNbd0401, {out: {inline : 1}}))
+printjson(db.people.mapReduce(mapFunctionNbd0401, reduceFunctionNbd0401, {out: {inline: 1}}))
